32fc7d6a3b5706987547909e6fadbe6a3fa13d1b
Abhik <abhiksetia@gmail.com>
2017-12-18 02:22:49 +0530

    Added carousal

--- js/main.js
+++ js/main.js
@@ -5,3 +5,6 @@
       
     }); // end of document ready
   })(jQuery); // end of jQuery name space
+
+  $('.carousel.carousel-slider').carousel({fullWidth: true,indicators:true,noWrap:true});
+  $('.carousel.carousel-slider').carousel();
