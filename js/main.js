890ad57a21f90036728a6df3d7b21ddd44aae028
Abhik <abhiksetia@gmail.com>
2017-12-18 02:39:52 +0530

    Added carousal animation and sidebar text

--- js/main.js
+++ js/main.js
@@ -6,5 +6,13 @@
     }); // end of document ready
   })(jQuery); // end of jQuery name space
 
-  $('.carousel.carousel-slider').carousel({fullWidth: true,indicators:true,noWrap:true});
-  $('.carousel.carousel-slider').carousel();
+  $('.carousel.carousel-slider').carousel({fullWidth: true,indicators:true,noWrap:false});
+  
+  $(document).ready(function(){
+    setTimeout(changeSlide,5000);
+    });
+
+  function changeSlide(){
+    $('.carousel.carousel-slider').carousel('next');  
+    setTimeout(changeSlide,4000);
+  }
