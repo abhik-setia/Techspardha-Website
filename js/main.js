97710002771974e3a55203b2ab92aa331ef90fcc
Abhik <abhiksetia@gmail.com>
2017-12-20 22:40:58 +0530

    adding changes carousal,scrollspy not working

--- js/main.js
+++ js/main.js
@@ -6,11 +6,13 @@
     }); // end of document ready
   })(jQuery); // end of jQuery name space
 
-  $('.carousel.carousel-slider').carousel({fullWidth: true,indicators:true,noWrap:false});
+  $('.carousel.carousel-slider').carousel({fullWidth: false,indicators:true,noWrap:false});
   
   $(document).ready(function(){
     setTimeout(changeSlide,5000);
-    });
+    $(".dropdown-button").dropdown();
+    $('.scrollspy').scrollSpy();
+   });
 
   function changeSlide(){
     $('.carousel.carousel-slider').carousel('next');  
@@ -20,4 +22,12 @@
   $(document).ready(function(){
     $('.events_tab').tabs({'swipeable':true});
   });
-        
+var options = [
+    {selector: '#section_aboutus', offset: 0, callback: function() {
+        console.log("Scrolled");
+    } },
+    {selector: '#section_events', offset: 0, callback: function(){
+        Materialize.toast("Add fade in animation!", 1500 );
+    } },
+  ];
+Materialize.scrollFire(options);
