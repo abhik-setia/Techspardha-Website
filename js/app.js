88c4ecae6a07faada8eb84006e0f15e426d9bc2f
Abhik <abhiksetia@gmail.com>
2017-12-21 16:07:10 +0530

    updating branch

--- js/app.js
+++ js/app.js
@@ -3,11 +3,10 @@
 /* ----------------------------------------------- */
 
 /* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
-/*
+
 particlesJS.load('particles-js', 'particles.json', function() {
   console.log('particles.js loaded - callback');
 });
-*/
 
 /* Otherwise just put the config content (json): */
 
