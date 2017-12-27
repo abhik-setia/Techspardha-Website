fc27749f37e0209b15fdc240c52282f39472d079
sahil070197 <sahil.070197gmail.com1>
2017-12-27 13:44:50 +0530

    Added Astronomy

--- public/js/exhibition.js
+++ public/js/exhibition.js
@@ -1,7 +1,8 @@
-$(document).ready(function () {
-  $(".gallery-img").click(function(){
-    var t = $(this).attr("src");
-    $(".modal-body").html("<img src='"+t+"' class='modal-img'>");
-    $("#myModal").modal();
-  });
-});
+ function exhibiton_inflator(){
+     
+     
+     var data="";
+     var exhibition_holder=$('#exhibition_placeholder');
+     
+     <img src="https://img.buzzfeed.com/buzzfeed-static/static/2015-08/11/14/enhanced/webdr01/original-9161-1439317330-3.jpg?downsize=715:*&output-format=auto&output-quality=auto" alt="" width="100%" height="auto" class="gallery-img" onclick="blurin();"/>o
+}
