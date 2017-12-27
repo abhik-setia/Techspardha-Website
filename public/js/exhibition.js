3b91b735db651b0119b507acb0ada0b9e34dd965
sahil070197 <sahil.070197gmail.com1>
2017-12-27 15:21:51 +0530

    Dynamic Exhibition

--- public/js/exhibition.js
+++
@@ -1,8 +0,0 @@
- function exhibiton_inflator(){
-     
-     
-     var data="";
-     var exhibition_holder=$('#exhibition_placeholder');
-     
-     <img src="https://img.buzzfeed.com/buzzfeed-static/static/2015-08/11/14/enhanced/webdr01/original-9161-1439317330-3.jpg?downsize=715:*&output-format=auto&output-quality=auto" alt="" width="100%" height="auto" class="gallery-img" onclick="blurin();"/>o
-}
--- public/js/index_database/exhibition-downloader.js
+++ public/js/index_database/exhibition-downloader.js
@@ -1,18 +1,51 @@
+var exhibition_feed=new Map();
+
+$(function() {
+  $('#overlay').removeClass('blur-in');
+});
+
+function blurin(index)
+{
+    console.log("Click", index);
+    $('.pop-up').css('visibility','visible');
+    $('#close_popup').css('visibility','visible');
+    $('.pop-up').fadeIn(300);
+    $('#overlay').addClass('blur-in');
+    $('#overlay').removeClass('blur-out');
+    $('#close_popup').fadeIn(100);
+    $('.exhibition_popup_name').append(exhibition_feed[index].heading);
+    $('#exhibition_popup_image').attr('src',exhibition_feed[index].imageurl+'.jpg');
+    $('#exhibition_popup_description').append(exhibition_feed[index].text);
+    
+}
+function close_popup()
+{
+    $('.pop-up').fadeOut(300);
+    $('#overlay').removeClass('blur-in');
+    $('#overlay').addClass('blur-out');
+    $('#close_popup').fadeOut(100);
+}
 function getExhibitonFeeds()
 {
-    var exhibition_feed=[];
     exhibitionid.once('value', function(snapshot){
+        
         var exhibition_feed_pair=snapshot.val();
         var items=exhibition_feed_pair.length -1; //1 Indexed
         snapshot.forEach(function(priority_id_pair)
         {
                 exhibitionitem.child(priority_id_pair.val()).once('value', function(feed){
+                console.log("Feed", feed.val(), priority_id_pair.key);
                 exhibition_feed[priority_id_pair.key]=feed.val();
+                exhibiton_inflator(priority_id_pair.key);    
             });
         });
-        console.log("Exhibition Feeds: ",exhibition_feed);
     });
-    return exhibition_feed;
 }
 
+function exhibiton_inflator(i){ 
+    var exhibition_holder=$('#exhibition_placeholder');
+    console.log("Feedsss ", i, " ", exhibition_feed[i]);
+    var data='<img src="'+exhibition_feed[i].imageurl+'.jpg" alt="" width="100%" height="auto" class="gallery-img" onclick="blurin('+i+');"/>';
+    exhibition_holder.append(data);
+}
 getExhibitonFeeds();
