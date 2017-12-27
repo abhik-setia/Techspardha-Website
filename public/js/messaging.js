1d580c0d1d167ff8e89b89bb06945942c2597fa8
sahil070197 <sahil.070197gmail.com1>
2017-12-28 01:05:53 +0530

    Not working post

--- public/js/messaging.js
+++ public/js/messaging.js
@@ -1,23 +1,33 @@
 const messaging=firebase.messaging();
 function addDeviceToGroup(token)
 {
+    console.log(token);
+   
     $.ajax({
-    url: 'https://android.googleapis.com/gcm/notification',
-    type: 'post',
-    headers: {
-        Authorization:'key=AIzaSyD1aqciM48jQXz-yv4W_0mkFCicWsJ7Gpo',
-        project_id:'357922932377'
-    },
+  type: 'POST',
+
+  url: 'https://android.googleapis.com/gcm/notification',
+  contentType: 'application/json',
+  dataType:'json',
+  
+  headers : {
+    Authorization:'key=AIzaSyD1aqciM48jQXz-yv4W_0mkFCicWsJ7Gpo',
+    project_id:357922932377
+  },
     data:{
        "operation": "add",
-       "notification_key": "APA91bEmZIrHf4hF99b4H14lgrfaEG-4ImiKTo-QxU1mdD_suPTkLgkFo7C1WOIX7bopvBGnlrdQbJMPqT_3B_531YVVd0q19mHEIiqeFQYQsT3ywwwt0z0",
-       "registration_ids": [token]
+	   "notification_key_name": "xxxxx",
+        "notification_key": "APA91bEmZIrHf4hF99b4H14lgrfaEG-4ImiKTo-QxU1mdD_suPTkLgkFo7C1WOIX7bopvBGnlrdQbJMPqT_3B_531YVVd0q19mHEIiqeFQYQsT3ywwwt0z0",
+        "registration_ids": ["f_iKw_kuFiI:APA91bE5RkRdvd9_yE7j_UVO1nPZcq7yQv1OmEI3PwETbWMlha-v409rWplBi7yY8a0xPxgdIqJQ84JZ3Rha9RUOl8xH9EFnw_g0xJZfCERsJ6Pf8CvTyXdDMP8-a1Cv9PmFyJY8ddS2"]
     },
-    dataType: 'json',
-    success: function (data) {
-        console.log(data);
-        }
-    });
+  success: function(data) {
+    console.log(data)
+  },
+
+  error: function() {
+    
+  }
+});
 }
 function askPermission()
 {
