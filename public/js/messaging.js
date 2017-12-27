0449314c1b3fa0677915c19cbc8b4c79139f387d
sahil070197 <sahil.070197gmail.com1>
2017-12-28 01:33:17 +0530

    Sender working

--- public/js/messaging.js
+++ public/js/messaging.js
@@ -2,32 +2,58 @@ const messaging=firebase.messaging();
 function addDeviceToGroup(token)
 {
     console.log(token);
-   
-    $.ajax({
-  type: 'POST',
-
-  url: 'https://android.googleapis.com/gcm/notification',
-  contentType: 'application/json',
-  dataType:'json',
-  
-  headers : {
-    Authorization:'key=AIzaSyD1aqciM48jQXz-yv4W_0mkFCicWsJ7Gpo',
-    project_id:357922932377
+   var notificationData = {
+  "to": "APA91bEmZIrHf4hF99b4H14lgrfaEG-4ImiKTo-QxU1mdD_suPTkLgkFo7C1WOIX7bopvBGnlrdQbJMPqT_3B_531YVVd0q19mHEIiqeFQYQsT3ywwwt0z0",
+  "data": {
+    "mrp": 5000,
+    "retailPrice": 3000
   },
-    data:{
-       "operation": "add",
-	   "notification_key_name": "xxxxx",
-        "notification_key": "APA91bEmZIrHf4hF99b4H14lgrfaEG-4ImiKTo-QxU1mdD_suPTkLgkFo7C1WOIX7bopvBGnlrdQbJMPqT_3B_531YVVd0q19mHEIiqeFQYQsT3ywwwt0z0",
-        "registration_ids": ["f_iKw_kuFiI:APA91bE5RkRdvd9_yE7j_UVO1nPZcq7yQv1OmEI3PwETbWMlha-v409rWplBi7yY8a0xPxgdIqJQ84JZ3Rha9RUOl8xH9EFnw_g0xJZfCERsJ6Pf8CvTyXdDMP8-a1Cv9PmFyJY8ddS2"]
-    },
-  success: function(data) {
-    console.log(data)
-  },
-
-  error: function() {
-    
+  "notification": {
+    "color": "#FF0000",
+    "title": "Off Upto 70% yofunky.com"
   }
-});
+}
+
+$.ajax({
+    url: 'https://fcm.googleapis.com/fcm/send',
+    type: 'post',
+    data: JSON.stringify(notificationData),
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'key=AIzaSyD1aqciM48jQXz-yv4W_0mkFCicWsJ7Gpo'
+    },
+    dataType: 'json',
+    success: function (data) {
+      console.info(data);
+    }
+  });
+ 
+    
+//    $.ajax({
+//  type: 'POST',
+//  url: 'https://fcm.googleapis.com/fcm/send',
+//  contentType: 'application/json',
+//  dataType:'json',
+//  
+//  headers : {
+//    Authorization:'key=AIzaSyD1aqciM48jQXz-yv4W_0mkFCicWsJ7Gpo',
+//    project_id:357922932377
+//  },
+//    data:{
+//        "to":"APA91bEmZIrHf4hF99b4H14lgrfaEG-4ImiKTo-QxU1mdD_suPTkLgkFo7C1WOIX7bopvBGnlrdQbJMPqT_3B_531YVVd0q19mHEIiqeFQYQsT3ywwwt0z0",
+//        "notification":{
+//            "body":"Hello World",
+//            "title":"FCM MESSAGE"
+//        }
+//    },
+//  success: function(data) {
+//    console.log(data)
+//  },
+//
+//  error: function() {
+//    
+//  }
+//});
 }
 function askPermission()
 {
