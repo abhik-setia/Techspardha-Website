2db31450328a390aeddf0a86e113768a09a935d0
sahil070197 <sahil.070197gmail.com1>
2017-12-28 11:27:15 +0530

    Please wait

--- public/js/messaging.js
+++ public/js/messaging.js
@@ -29,23 +29,28 @@ function addDeviceToGroup(token)
 //  });
     
     console.log(token);
-   var notificationData = {
-  "data": { "operation": "add",
-	   "notification_key_name": "xxxxx",
-        "notification_key": "APA91bEmZIrHf4hF99b4H14lgrfaEG-4ImiKTo-QxU1mdD_suPTkLgkFo7C1WOIX7bopvBGnlrdQbJMPqT_3B_531YVVd0q19mHEIiqeFQYQsT3ywwwt0z0",
-        "registration_ids": ["f_iKw_kuFiI:APA91bE5RkRdvd9_yE7j_UVO1nPZcq7yQv1OmEI3PwETbWMlha-v409rWplBi7yY8a0xPxgdIqJQ84JZ3Rha9RUOl8xH9EFnw_g0xJZfCERsJ6Pf8CvTyXdDMP8-a1Cv9PmFyJY8ddS2"]
-}
-   }
+//   var notificationData = {
+//  "data": { "operation": "add",
+//	   "notification_key_name": "xxxxx",
+//        "notification_key": "APA91bEmZIrHf4hF99b4H14lgrfaEG-4ImiKTo-QxU1mdD_suPTkLgkFo7C1WOIX7bopvBGnlrdQbJMPqT_3B_531YVVd0q19mHEIiqeFQYQsT3ywwwt0z0",
+//        "registration_ids": ["f_iKw_kuFiI:APA91bE5RkRdvd9_yE7j_UVO1nPZcq7yQv1OmEI3PwETbWMlha-v409rWplBi7yY8a0xPxgdIqJQ84JZ3Rha9RUOl8xH9EFnw_g0xJZfCERsJ6Pf8CvTyXdDMP8-a1Cv9PmFyJY8ddS2"]
+//}
+//   }
 
 $.ajax({
     url: 'https://android.googleapis.com/gcm/notification',
     type: 'post',
-    data: JSON.stringify(notificationData),
+    data: { 
+        "operation": "add",
+	   "notification_key_name": "xxxxx",
+        "notification_key": "APA91bEmZIrHf4hF99b4H14lgrfaEG-4ImiKTo-QxU1mdD_suPTkLgkFo7C1WOIX7bopvBGnlrdQbJMPqT_3B_531YVVd0q19mHEIiqeFQYQsT3ywwwt0z0",
+        "registration_ids": ["f_iKw_kuFiI:APA91bE5RkRdvd9_yE7j_UVO1nPZcq7yQv1OmEI3PwETbWMlha-v409rWplBi7yY8a0xPxgdIqJQ84JZ3Rha9RUOl8xH9EFnw_g0xJZfCERsJ6Pf8CvTyXdDMP8-a1Cv9PmFyJY8ddS2"]
+    },
     headers: {
         'Content-Type': 'application/json',
-        'Authorization': 'key=AIzaSyD1aqciM48jQXz-yv4W_0mkFCicWsJ7Gpo',
+        Authorization: 'key=AIzaSyD1aqciM48jQXz-yv4W_0mkFCicWsJ7Gpo',
+        project_id: 357922932377
     },
-    dataType: 'json',
     success: function (data) {
       console.info(data);
     }
