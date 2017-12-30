4614828e6eac5782cb7ed653370f324cb8d95fc1
sahil070197 <sahil.070197gmail.com1>
2017-12-30 15:44:34 +0530

    Queries Complete

--- public/js/index_database/contact.js
+++ public/js/index_database/contact.js
@@ -1,20 +1,35 @@
 function getQueries()
 {
     var queries=new Object();
-    query.on('value', function(snapshot){
+    var ref=$('#query_table');
+    var index=0;
+    query.once('value', function(snapshot){
         snapshot.forEach(function(id_query_pair)
         {
-            queries[id_query_pair.key]=id_query_pair.val();     
+            index++;
+            var q=id_query_pair.val();
+            var k=id_query_pair.key;
+            queries[k]=q;
+            var data='<tr id='+k+'>\
+                    <td>'+index+'</td>\
+                    <td>'+q.name+'</td>\
+                    <td>'+q.email+'</td>\
+                    <td>'+q.query+'</td>\
+                    <td>\
+                        <div class="btn btn-floating" onclick="delete_query(\''+k+'\');"><i class="material-icons">check</i></div>\
+                    </td>\
+                    </tr>';
+            ref.append(data);
         });
         console.log("Queries : ",queries);
     });
-//    return queries;
 }
 
-getQueries();
 
 function delete_query(query_id)
 {
+    $('#'+query_id).css('display', 'none');
     query.child(query_id).remove();
 }
+getQueries();
 
